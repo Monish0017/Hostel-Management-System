65f542bde5956b06e2842fd89847e2b23a02dcde
Kabhinyasri <kabhinyasriselvaraj@gmail.com>
2024-09-26 00:26:50 +0530

    work done2

--- client-app/src/Pages/StudentApplication.jsx
+++ client-app/src/Pages/StudentApplication.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './CSS/StudentApplication.css';
+import profile from '../assets/profile.jpg';
+
 
 const StudentApplication = () => {
   return (
@@ -9,6 +11,18 @@ const StudentApplication = () => {
         <button className="confirm-button">Confirm Student</button>
         <button className="delete-button">Delete Student</button>
       </div>
+      <div className="student-grid">
+              <div className="student-box">
+                <img src={profile} alt="Profile" className="profile-picture"/>
+                <p>Full Name: Monish</p>
+                <p>Roll No: 22z240</p>
+              </div>
+              <div className="student-box">
+                <img src={profile} alt="Profile" className="profile-picture"/>
+                <p>Full Name: Kabhinya</p>
+                <p>Roll No: 22z229</p>
+              </div>
+        </div>
     </div>
   );
 }
