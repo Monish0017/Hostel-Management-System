660020c594c57584eaa33bf1c71645728c213295
Monish0017 <22z240@psgtech.ac.in>
2024-09-29 22:39:12 +0530

    pac-1

--- Server/server.js
+++ Server/server.js
@@ -15,8 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
