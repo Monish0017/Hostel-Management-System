7b7f632bf7a4b993615719ed9bed8eacbb826b9e
Monish0017 <22z240@psgtech.ac.in>
2024-09-30 00:18:59 +0530

    P

--- Server/server.js
+++ Server/server.js
@@ -15,10 +15,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
-
 
 // Importing routes
 const studentRoutes = require('./routes/studentRoutes');
@@ -29,7 +29,6 @@ const adminRoutes = require('./routes/adminRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const studentApplicationRoutes = require('./routes/stduentApplicationRoutes');
-// const ivrsRoutes = require('./routes/ivrsRoutes');
 
 // Use routes
 app.use('/students', studentRoutes);
@@ -40,7 +39,14 @@ app.use('/admin', adminRoutes);
 app.use('/api', applicationRoutes);
 app.use('/payments', paymentRoutes);
 app.use('/application', studentApplicationRoutes);
-// app.use('/api/ivrs', ivrsRoutes);
 
-// Export the app instead of listening
-module.exports = app;
+// Health check route
+app.get('/', (req, res) => {
+    res.send('Hostel Management System API is running');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000; // Use dynamic port for deployment
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
