56b88c15a8a632d9f21884d61817194d35bc9725
Kabhinyasri <kabhinyasriselvaraj@gmail.com>
2024-10-01 15:34:43 +0530

    Your commit message here

--- Server/server.js
+++ Server/server.js
@@ -5,40 +5,48 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Use CORS middleware
+app.use(bodyParser.urlencoded({ extended: true })); // Ensure this is true or false based on your need
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
+// Serve static files
 app.use('/Assets', express.static(path.join(__dirname, 'Assets')));
-// Import routes
-const studentRoutes = require('./routes/studentRoutes');//Over
+
+// Importing routes
+const studentRoutes = require('./routes/studentRoutes');
 const foodRoutes = require('./routes/foodRoutes');
 const facultyRoutes = require('./routes/facultyRoutes');
 const authRoutes = require('./routes/authRoutes');
-const adminRoutes = require('./routes/adminRoutes'); // Add this line
+const adminRoutes = require('./routes/adminRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
+const studentApplicationRoutes = require('./routes/stduentApplicationRoutes');
+// const ivrsRoutes = require('./routes/ivrsRoutes');
 
 // Use routes
 app.use('/students', studentRoutes);
 app.use('/food', foodRoutes);
 app.use('/faculty', facultyRoutes);
 app.use('/auth', authRoutes);
-app.use('/admin', adminRoutes); // Add this line
+app.use('/admin', adminRoutes);
 app.use('/api', applicationRoutes);
-app.use('/payments' , paymentRoutes);
+app.use('/payments', paymentRoutes);
+app.use('/application', studentApplicationRoutes);
+// app.use('/api/ivrs', ivrsRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Start the server
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${ port }`);
 });
