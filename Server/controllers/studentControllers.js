fa39f199c738e43af1f6cf3cb836ec106c5cde39
Monish0017 <22z240@psgtech.ac.in>
2024-10-28 01:09:54 +0530

    P

--- Server/controllers/studentControllers.js
+++ Server/controllers/studentControllers.js
@@ -101,7 +101,7 @@ const forgotPassword = async (req, res) => {
     student.resetPasswordExpires = Date.now() + 3600000; // 1 hour
     await student.save();
 
-    const resetUrl = `https://hostel-management-system-api.onrender.com/reset-password/${token}`;
+    const resetUrl = `https://hostel-management-system-xi.vercel.app/reset-password/${token}`;
     const mailOptions = {
       from: `"Hostel Management System" <${process.env.EMAIL_USER}>`,
       to: student.email,
